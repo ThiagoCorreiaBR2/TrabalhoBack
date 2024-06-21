beda5e77e617b156a237714f6f4efe7f73bc1b3a
ThiagoCorreiaBR2 <136650726+ThiagoCorreiaBR2@users.noreply.github.com>
2024-06-21 19:38:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const options={
         },
         servers: [
             {
-                url: 'http://localhost:3000',
+                url: 'https://trabalho-back.vercel.app/api-docs',
             },
         ],
     },
